6e5479ad5ae016af07fa9e1b4eefef5e2350272a
Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
2021-04-28 13:14:27 +0200

    Auto-refresh zipcube wiz when measurement is done

--- stock_measuring_device_zippcube/components/zippcube_device_component.py
+++ stock_measuring_device_zippcube/components/zippcube_device_component.py
@@ -3,8 +3,6 @@
 
 import logging
 
-from odoo import _
-
 from odoo.addons.component.core import Component
 
 _logger = logging.getLogger(__name__)
@@ -39,4 +37,5 @@ class ZippcubeDevice(Component):
         # device wizard.
         if wizard_line:
             wizard_line.wizard_id._notify(_("Please, press the REFRESH button."))
+        wizard_line.wizard_id._send_notification_refresh()
         packaging._measuring_device_release()
