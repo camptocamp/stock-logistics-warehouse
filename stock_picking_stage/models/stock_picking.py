fe8c1cca3dc26190679e9a25bc928febba447a29
Hadrien Huvelle <github.com@alphawebcreation.be>
2025-03-04 17:58:00 +0100

    [IMP] rename fields according to Ricardo's comment

--- stock_picking_stage/models/stock_picking.py
+++ stock_picking_stage/models/stock_picking.py
@@ -8,6 +8,6 @@ from odoo import fields, models
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    picking_stage_id = fields.Many2one("stock.picking.stage", index=True, tracking=True)
-    picking_stage_code = fields.Char(related="picking_stage_id.code")
-    picking_stage_color = fields.Integer(related="picking_stage_id.color")
+    stage_id = fields.Many2one("stock.picking.stage", index=True, tracking=True)
+    stage_code = fields.Char(related="picking_stage_id.code")
+    stage_color = fields.Integer(related="picking_stage_id.color")
